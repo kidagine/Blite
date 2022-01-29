98a979f781be2bf9e46136b7c7cdab6b150310b0
kidagine <davidkalatzis1999@hotmail.com>
2022-01-29 19:37:08 +0100

    update

--- Assets/_Project/Scripts/MiscScripts/ActivationButton.cs
+++ Assets/_Project/Scripts/MiscScripts/ActivationButton.cs
@@ -2,9 +2,11 @@ using Demonics.Sounds;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ActivationButton : MonoBehaviour
 {
+	[SerializeField] private UnityEvent _onActivate = default;
 	[SerializeField] private Sprite _activeButtonSprite = default;
 	[SerializeField] private Sprite _inactiveButtonSprite = default;
 	private SpriteRenderer _spriteRenderer;
@@ -24,6 +26,7 @@ public class ActivationButton : MonoBehaviour
 		{
 			if (collision.TryGetComponent(out Projectile projectile))
 			{
+				_onActivate?.Invoke();
 				_hasActivated = true;
 				_audio.Sound("ButtonActivate").Play();
 				_spriteRenderer.sprite = _inactiveButtonSprite;
